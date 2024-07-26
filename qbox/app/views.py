68db29a83d89eb7f9be5e281bf55bcf059216cec
Mc Nigno <mcnigno@gmail.com>
2024-07-26 10:55:02 +0200

    request by and on

--- qbox/app/views.py
+++ qbox/app/views.py
@@ -466,9 +466,9 @@ class VolumeProView(ModelView):
     list_title = 'List Documents'
     
     list_columns = ['project.code', 'name','group']
-    add_columns = ['box','project','type','group', 'name','date_start','date_end','available','active']
-    show_columns = ['box','box.section.area.site','box.section.area','box.section','project.account','project', 'type', 'group', 'name','request_by','date_start','date_end','activation_date','endlife_date','available','active']
-    edit_columns = ['box','project','type','group', 'name','date_start','date_end','activation_date','available','active'] 
+    add_columns = ['box','project','type','group', 'name','date_start','date_end','request_by','request_on','available','active']
+    show_columns = ['box','box.section.area.site','box.section.area','box.section','project.account','project', 'type', 'group', 'name','request_by','request_on','date_start','date_end','activation_date','endlife_date','available','active']
+    edit_columns = ['box','project','type','group', 'name','date_start','date_end','activation_date','request_by','request_on','available','active'] 
     
     
     label_columns = {
